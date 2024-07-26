4c73282419aa7ab009b59a5a7514d303998c9f01
Mintimate <mintimate215@gmail.com>
2024-07-27 00:36:18 +0800

    style: 更新donate样式

--- .vitepress/theme/components/donate.vue
+++ .vitepress/theme/components/donate.vue
@@ -10,10 +10,10 @@ const props = defineProps({
 <template>
   <details class="details custom-block">
     <summary>
-      {{
+      <span class="title" v-text="
         lang === 'zh' ? '如果认为本文档或者薄荷拼音对你很有帮助，可以请我喝咖啡 ☕' :
             'If you find this document or Oh-my-rime Pinyin useful, you can buy me a coffee ☕'
-      }}
+      "/>
     </summary>
     <p>
       <img style="max-width: 300px;margin: auto;" alt="WebChart Recognise"
@@ -24,10 +24,39 @@ const props = defineProps({
 </template>
 
 <style scoped>
-img {
+.dark img {
   -webkit-filter: brightness(0.9);
   filter: brightness(0.9);
   transition: filter 0.2s ease-in-out;
+}
+
+img {
+  -webkit-filter: brightness(0.95);
+  filter: brightness(0.95);
   border-radius: 24px;
 }
+
+.title {
+  text-decoration: underline;
+  text-decoration-color: #41d1ff;
+  text-underline-offset: 5px;
+  text-decoration-style: wavy;
+  margin-left: 5px; /* 添加一些间距 */
+}
+
+
+details {
+  transition: max-height 0.8s ease-in-out;
+  overflow-y: hidden;
+}
+
+
+details[open] {
+  max-height: 380px;
+}
+
+details:not([open]) {
+  max-height: 100px;
+}
+
 </style>
