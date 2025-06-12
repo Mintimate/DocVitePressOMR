045cf6583c2539dc9dba524429f57722533ddd8e
Mintimate <mintimate215@gmail.com>
2025-06-12 23:51:56 +0800

    feat: 添加Vite配置支持WASM资源复制并启用ES模块

---
+++ vite.config.mjs
@@ -0,0 +1,19 @@
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: true
+  },
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: '.vitepress/theme/components/resources/fcitx5Online/Fcitx5.wasm',
+          dest: 'chunks'
+        }
+      ]
+    })
+  ]
+})
